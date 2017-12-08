f3cd3440c7a8f4867648e88849508cbc30a019a0
Yecheng Xu <xyc0562@gmail.com>
2017-12-08 11:17:56 +0800

    bugfix

--- lib/params_magic/config.rb
+++ lib/params_magic/config.rb
@@ -16,7 +16,7 @@ module ParamsMagic
 
   class Configuration #:nodoc:
     include ActiveSupport::Configurable
-    config_accessor :per_page_limit
+    config_accessor :per_page_limit, :root_key
 
     def param_name
       config.param_name.respond_to?(:call) ? config.param_name.call : config.param_name
